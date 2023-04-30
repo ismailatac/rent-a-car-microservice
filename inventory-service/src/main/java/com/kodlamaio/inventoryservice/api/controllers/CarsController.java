1b2e0755476cd1abc5e610566c1b72fd52306a65
ismailatac <atac571@outlook.com>
2023-04-30 16:49:17 +0300

    project created

--- inventory-service/src/main/java/com/kodlamaio/inventoryservice/api/controllers/CarsController.java
+++ inventory-service/src/main/java/com/kodlamaio/inventoryservice/api/controllers/CarsController.java
@@ -8,5 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/cars")
 public class CarsController {
-    //TODO: add operations
+    //TODO: add operation
 }
