5621c6af095a9cb9ed91e32001484fa6a85a83ae
ismailatac <atac571@outlook.com>
2023-05-18 15:25:13 +0300

    rental service module updated

--- rental-service/src/main/java/com/kodlamaio/rentalservice/business/concretes/RentalManager.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/business/concretes/RentalManager.java
@@ -1,6 +1,7 @@
 package com.kodlamaio.rentalservice.business.concretes;
 
-
+import com.kodlamaio.commonpackage.events.rental.RentalCreatedEvent;
+import com.kodlamaio.commonpackage.events.rental.RentalDeletedEvent;
 import com.kodlamaio.commonpackage.utils.mappers.ModelMapperService;
 import com.kodlamaio.rentalservice.business.abstracts.RentalService;
 import com.kodlamaio.rentalservice.business.dto.requests.CreateRentalRequest;
@@ -9,63 +10,86 @@ import com.kodlamaio.rentalservice.business.dto.responses.CreateRentalResponse;
 import com.kodlamaio.rentalservice.business.dto.responses.GetAllRentalsResponse;
 import com.kodlamaio.rentalservice.business.dto.responses.GetRentalResponse;
 import com.kodlamaio.rentalservice.business.dto.responses.UpdateRentalResponse;
+import com.kodlamaio.rentalservice.business.rules.RentalBusinessRules;
 import com.kodlamaio.rentalservice.entities.Rental;
 import com.kodlamaio.rentalservice.repository.RentalRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 public class RentalManager implements RentalService {
-
-    private final ModelMapperService mapper;
     private final RentalRepository repository;
+    private final ModelMapperService mapper;
+    private final RentalBusinessRules rules;
+    private final com.kodlamaio.commonpackage.kafka.producer.KafkaProducer producer;
 
     @Override
     public List<GetAllRentalsResponse> getAll() {
-        List<Rental> rentals = repository.findAll();
-        List<GetAllRentalsResponse> response = rentals.stream()
-                .map(rental -> mapper.forResponse().map(rental, GetAllRentalsResponse.class)).toList();
+        var rentals = repository.findAll();
+        var response = rentals
+                .stream()
+                .map(rental -> mapper.forResponse().map(rental, GetAllRentalsResponse.class))
+                .toList();
+
         return response;
     }
 
     @Override
     public GetRentalResponse getById(UUID id) {
-        //rules.checkIfRentalExists(id);
-        Rental rental = repository.findById(id).orElseThrow();
-        return mapper.forResponse().map(rental, GetRentalResponse.class);    }
+        rules.checkIfRentalExists(id);
+        var rental = repository.findById(id).orElseThrow();
+        var response = mapper.forResponse().map(rental, GetRentalResponse.class);
+
+        return response;
+    }
 
     @Override
     public CreateRentalResponse add(CreateRentalRequest request) {
-        //rules.checkIfRentalExistsByName(request.getName());
-        Rental rentalSave = mapper.forRequest().map(request, Rental.class);
-        rentalSave.setId(null);
-        rentalSave.setTotalPrice(getTotalPrice(rentalSave));
-        rentalSave.setRentedAt(LocalDate.now());
-        repository.save(rentalSave);
-        return mapper.forResponse().map(rentalSave, CreateRentalResponse.class);
+        rules.ensureCarIsAvailable(request.getCarId());
+        var rental = mapper.forRequest().map(request, Rental.class);
+        rental.setId(null);
+        rental.setTotalPrice(getTotalPrice(rental));
+        rental.setRentedAt(LocalDate.now());
+        repository.save(rental);
+        sendKafkaRentalCreatedEvent(request.getCarId());
+        var response = mapper.forResponse().map(rental, CreateRentalResponse.class);
+
+        return response;
     }
 
     @Override
     public UpdateRentalResponse update(UUID id, UpdateRentalRequest request) {
-        Rental updateRental = mapper.forRequest().map(request, Rental.class);
-        updateRental.setId(id);
-        repository.save(updateRental);
-        return mapper.forResponse().map(updateRental, UpdateRentalResponse.class);
+        rules.checkIfRentalExists(id);
+        var rental = mapper.forRequest().map(request, Rental.class);
+        rental.setId(id);
+        repository.save(rental);
+        var response = mapper.forResponse().map(rental, UpdateRentalResponse.class);
+
+        return response;
     }
 
     @Override
     public void delete(UUID id) {
-        //rules.checkIfRentalExists(id);
+        rules.checkIfRentalExists(id);
+        sendKafkaRentalDeletedEvent(id);
         repository.deleteById(id);
     }
+
     private double getTotalPrice(Rental rental) {
         return rental.getDailyPrice() * rental.getRentedForDays();
     }
 
+    private void sendKafkaRentalCreatedEvent(UUID carId) {
+        producer.sendMessage(new RentalCreatedEvent(carId), "rental-created");
+    }
+
+    private void sendKafkaRentalDeletedEvent(UUID id) {
+        var carId = repository.findById(id).orElseThrow().getCarId();
+        producer.sendMessage(new RentalDeletedEvent(carId), "rental-deleted");
+    }
 }
--- rental-service/src/main/java/com/kodlamaio/rentalservice/business/rules/RentalBusinessRules.java
+++ rental-service/src/main/java/com/kodlamaio/rentalservice/business/rules/RentalBusinessRules.java
@@ -1,26 +1,29 @@
-//package com.kodlamaio.rentalservice.business.rules;
-//
-//
-//import com.kodlamaio.commonpackage.utils.exceptions.BusinessException;
-//import com.kodlamaio.inventoryservice.entities.enums.State;
-//import com.kodlamaio.rentalservice.repository.RentalRepository;
-//import lombok.AllArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@AllArgsConstructor
-//public class RentalBusinessRules {
-//    private final RentalRepository repository;
-//
-//    public void checkIfRentalExists(int id) {
-//        if (!repository.existsById(id)) {
-//            throw new BusinessException(Messages.Rental.NotExists);
-//        }
-//    }
-//
-//    public void checkIfCarAvailable(State state) {
-//        if (!state.equals(State.Available)) {
-//            throw new BusinessException(Messages.Car.NotAvailable);
-//        }
-//    }
-//}
+package com.kodlamaio.rentalservice.business.rules;
+
+import com.kodlamaio.commonpackage.utils.exceptions.BusinessException;
+import com.kodlamaio.rentalservice.api.clients.CarClient;
+import com.kodlamaio.rentalservice.repository.RentalRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class RentalBusinessRules {
+    private final RentalRepository repository;
+    private final CarClient carClient;
+
+    public void checkIfRentalExists(UUID id) {
+        if (!repository.existsById(id)) {
+            throw new BusinessException("RENTAL_NOT_EXISTS");
+        }
+    }
+
+    public void ensureCarIsAvailable(UUID carId) {
+        var response = carClient.checkIfCarAvailable(carId);
+        if (!response.isSuccess()) {
+            throw new BusinessException(response.getMessage());
+        }
+    }
+}
